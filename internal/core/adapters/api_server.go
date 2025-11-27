ab2f4ec116732d69382991c3c842b79aae98668b
suni.o <sunimaldv@gmail.com>
2025-11-28 00:04:53 +1100

    adding the missing methods to the interface

--- internal/core/adapters/api_server.go
+++ internal/core/adapters/api_server.go
@@ -5,4 +5,7 @@ import "net/http"
 type APIServer interface {
 	RegisterRoutes() http.Handler
 	HealthCheck(w http.ResponseWriter, r *http.Request)
+	ListProducts(w http.ResponseWriter, r *http.Request)
+	FindProductByID(w http.ResponseWriter, r *http.Request)
+	PlaceAnOrder(w http.ResponseWriter, r *http.Request)
 }
